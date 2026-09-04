f9e338ee7e407805180974132fa9bed8f64ac3e9
Rafael-0809 <rafael.lavino.cabral@escola.pr.gov.br>
2026-09-04 09:11:02 -0300

    Update testes.js
    Erro em teste.js corrigido

--- Rafael/testes.js
+++ Rafael/testes.js
@@ -1,58 +1,43 @@
 function executarTestes() {
-
     const resultado =
         document.getElementById("resultado");
 
     resultado.innerHTML = "";
 
-
     const testes = [
-
         {
             nome: "Teste de login",
             resultado: "OK"
         },
-
         {
             nome: "Teste de pesquisa",
             resultado: "OK"
         },
-
         {
             nome: "Teste de filtro",
             resultado: "OK"
         },
-
         {
             nome: "Teste de envio de dúvida",
             resultado: "OK"
         },
-
         {
             nome: "Teste de resposta do professor",
             resultado: "OK"
         }
-
     ];
 
 
     testes.forEach(function(teste) {
-
         const div =
             document.createElement("div");
-
         div.classList.add("teste");
-
         div.innerHTML = `
-
             <strong>
                 ${teste.nome}
             </strong>
-
             — ${teste.resultado}
-
         `;
-
         resultado.appendChild(div);
 
     });
